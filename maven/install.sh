86b4072aecbbc86d6778d672feee6d257fbb3245
root <root@ip-172-31-4-11.us-east-2.compute.internal>
2022-05-27 09:50:27 +0000

    update

--- maven/install.sh
+++ maven/install.sh
@@ -6,28 +6,30 @@ sudo yum install unzip -y
 #install java-11
 sudo yum install java-11-openjdk-devel -y
 #maven setup
-cd /usr/local/src
+cd /usr/local/src/
 sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz
 sudo tar -xvzf apache-maven-3.8.5-bin.tar.gz
 sudo mv apache-maven-3.8.5 apache-maven
 sudo rm -rf apache-maven-3.8.5-bin.tar.gz
-cd /etc/profile.d
+cd /etc/profile.d/
 touch maven.sh
 echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.15.0.10-2.el8_6.x86_64
 export M2_HOME=/usr/local/src/apache-maven
 export MAVEN_HOME=/usr/local/src/apache-maven
 export PATH=${M2_HOME}/bin:${PATH}" >> /etc/profile.d/maven.sh
+cd /etc/profile.d/
 sudo chmod +x maven.sh
 source maven.sh
 
 #sonarqube setup
 sudo - i
-cd /opt
+cd /opt/
 sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.8.zip
 sudo unzip sonarqube-7.8.zip
 sudo useradd sonar
 # Run me with superuser privileges
 sudo echo 'sonar  ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
+cd /opt/
 sudo chown -R sonar:sonar /opt/sonarqube-7.8/
 sudo chmod -R 775 /opt/sonarqube-7.8/
 su - sonar
